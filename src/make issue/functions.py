9431648d1d0bd0187338bc526848eb3cdf7991ed
chirag <whyiswhen@gmail.com>
2021-11-15 20:23:49 +0530

    Update functions.py

--- src/make issue/functions.py
+++ src/make issue/functions.py
@@ -16,13 +16,6 @@ def click_new_issue():
 
     pyautogui.click(x=25, y=100)
 
-# define a function that will click on the url bar at the top of the page
-
-
-def click_url_bar():
-
-    pyautogui.click(x=400, y=65)
-
 
 # define a function that will copy the url from the url bar using the pyautogui library by pressing the "ctrl" key + "c"
 def copyselectedtext():
@@ -33,40 +26,15 @@ def copyselectedtext():
 # type click on the body of the issue using pyautogui.click()
 def click_body():
 
-    pyautogui.click(x=605, y=575)
+    pyautogui.click(x=600, y=580)
 
 
 # click place of screenshot holder
 def click_screenshot_holder():
-    pyautogui.click(x=566, y=630)
-
-
-# move focus out of the comment box
-def move_focus_out_of_comment_box():
-    pyautogui.click(x=20, y=575)
-
-
-# define a function that will open the url in the default browser
-def open_url(url):
-
-    webbrowser.open(url)
-
-# define a function that will click at the specified coordinates
-
-
-def click_at_coordinates(x, y):
-
-    pyautogui.click(x=x, y=y)
-
-
-# define a function that will type the text
-def type_text(text):
-
-    pyautogui.typewrite(text)
+    pyautogui.click(x=600, y=630)
 
 
 def close_tab():
-
     pyautogui.hotkey('ctrl', 'w')
 
 
