c70d3dc90dbdefc01b770c4a48bc33e53ca28990
chirag <whyiswhen@gmail.com>
2022-05-28 17:25:49 +0530

    Create comment.py

---
+++ github/comment.py
@@ -0,0 +1,31 @@
+# Physical: {X=594,Y=609}
+# Physical: {X=1206,Y=745}
+
+import pyautogui
+from time import sleep
+
+def main():
+    # press end
+    pyautogui.press('end')
+
+    sleep(0.1)
+
+    pyautogui.click(594, 609)
+
+    sleep(0.1)
+
+    pyautogui.hotkey('ctrl', "v")
+
+    sleep(0.1)
+
+    pyautogui.click(1206, 745)
+
+    sleep(0.1)
+
+    pyautogui.hotkey('ctrl', "tab")
+
+if __name__ == '__main__':
+
+    sleep(5)
+    for _ in range(10):
+        main()
