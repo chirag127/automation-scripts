ca418b3f85120f35e267598c8cb70f208532d670
Chirag <76880977+chirag127@users.noreply.github.com>
2023-02-16 21:36:36 +0530

    adbloc to auto

--- search_all.py
+++ search_all.py
@@ -1,16 +1,71 @@
 import os
+import pyautogui
+from time import sleep
 import webbrowser
-string = """1. Burdman, Jessica, “Collaborative Web Development” Addison Wesley
-2. Xavier, C, “ Web Technology and Design” , New Age International
-3. Ivan Bayross,” HTML, DHTML, Java Script, Perl & CGI”, BPB Publication
-4. Bhave, “Programming with Java”, Pearson Education
-5. Herbert Schieldt, “The Complete Reference:Java”, McGraw Hill.
-6. Hans Bergsten, “Java Server Pages”, SPD O’Reilly
-7. Margaret Levine Young, “The Complete Reference Internet”, McGraw Hill.
-8. Naughton, Schildt, “The Complete Reference JAVA2”, McGraw Hill.
-9. Balagurusamy E, “Programming in JAVA”, McGraw Hill"""
 
-search_term = string.split("\n")
+
+
+
+# Insurance	$54.91
+# Loans	$44.28
+# Mortgage	$47.12
+# Attorney	$47.07
+# Credit	$36.06
+# Lawyer	$42.51
+# Donate	$42.02
+# Degree	$40.61
+# Hosting	$31.91
+# Claim	$45.51
+# Conference Call	$42.05
+# Trading	$33.19
+# Software	$35.29
+# Recovery	$42.03
+# Transfer	$29.86
+# Gas/Electicity	$54.62
+# Classes	$35.04
+# Rehab	$33.59
+# Treatment	$37.18
+# Cord Blood	$27.80
+
+string = """buy insurance online
+fartificial intelligence course
+course web development
+data science course
+course machine learning
+course python
+course java
+course javascript
+course c++
+c++ course
+course c#
+course c
+course c programming
+course c sharp
+course c language
+course c programming language
+course c programming tutorial
+course c programming for beginners
+best code assistant
+best ai copywriter
+best ai copywriting
+best ai writer
+best ai content writer
+best ai code assistant"""
+
+
+search_term = string.splitlines()
 for i in search_term:
 
-    webbrowser.open("https://neeva.com/search?q="+i+" book summary")
+    # webbrowser.open("https://neeva.com/search?q="+i+" book summary")
+
+    webbrowser.open("https://www.google.com/search?q="+i+" online&authuser=5")
+
+    sleep(5)
+
+    pyautogui.hotkey('ctrl', 'shift', 'tab')
+
+    sleep(5)
+
+    pyautogui.hotkey('ctrl', 'w')
+
+    sleep(1)
