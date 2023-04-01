e3acfe3ef82be94f07eae175733ea8d9361315ff
Chirag <76880977+chirag127@users.noreply.github.com>
2023-04-01 17:20:24 +0530

    ok

--- telegram/main.py
+++ telegram/main.py
@@ -2,35 +2,20 @@ from time import sleep
 import pyautogui
 import keyboard
 
+def right_click_and_select_option(x: int, y: int, option_x: int, option_y: int) -> None:
+    pyautogui.rightClick(x, y)
+    sleep(0.5)
+    pyautogui.click(option_x, option_y)
+    sleep(0.3)
+    pyautogui.click(1096, 565)
 
 def main() -> None:
-
     current_x, current_y = pyautogui.position()
-
-    pyautogui.rightClick(current_x, current_y)
-
-    sleep(0.5)
-
-    pyautogui.click(current_x + 103, current_y + 226)
-
-    sleep(0.3)
-
-    pyautogui.click(1096, 565)
-    sleep(0.3)
-
-
+    option_x, option_y = current_x + 103, current_y + 226
+    right_click_and_select_option(current_x, current_y, option_x, option_y)
     pyautogui.click(current_x, current_y)
-
-    sleep(0.3)
-
-
-    pyautogui.click(current_x, current_y)
-
-
-
 
 if __name__ == "__main__":
-
     while True:
         if keyboard.is_pressed("a"):
             main()
